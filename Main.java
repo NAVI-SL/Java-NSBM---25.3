014f3ac89b025836b7f6aab840ef652616cbc2c7
NAVI-SL <navithanavindul@gmail.com>
2026-04-28 16:11:57 +0530

    Add Main class for marks input and grading logic

---
+++ Main.java
@@ -0,0 +1,68 @@
+// Q1
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first Mark");
+        int q1 = scanner.nextInt();
+
+        System.out.println("Enter the second Mark");
+        int q2 = scanner.nextInt();
+
+        System.out.println("Enter the second Mark");
+        int q3 = scanner.nextInt();
+
+        System.out.println("Enter the fourth Mark");
+        int q4 = scanner.nextInt();
+        
+        int total = q1 + q2 + q3 + q4;
+        double average = total / 4.0;
+        
+        if(total >= 80){
+            System.out.println('A');
+        }
+        else if(total >= 65){
+            System.out.println('B');
+        }
+        else if (total >= 50){
+            System.out.println('C');
+        }
+        else if(total < 50){
+            System.out.println('F');
+        }
+        else{
+            System.out.println("Invalid Marks");
+        }
+
+        scanner.close();
+    }
+}
+
+// // Q2
+// public class lab2 {
+//     public static void main(String[] args) {
+//         int x = 7;
+//         int y = x++ + ++x + --x + x--;
+
+//         System.out.println("x = " + x + ", y = " + y);
+//     }
+// }
+
+// // Q3
+// import java.util.Scanner; 
+
+// public class Assignment1 {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+
+//         System.out.println("Enter Your Name: ");
+//         String name = scanner.nextLine();
+
+//         System.out.println("Hello " + name);
+
+//         scanner.close();
+//     }
+    
+// }
