386f60b3a589ab4d97766d3c1cfd40cdd0f87a68
NAVI-SL <navithanavindul@gmail.com>
2026-04-28 16:03:39 +0530

    Add lab2.java with commented-out code for Q2 exercise

---
+++ lab2.java
@@ -0,0 +1,12 @@
+// 
+
+
+// // Q2
+// public class lab2 {
+//     public static void main(String[] args) {
+//         int x = 7;
+//         int y = x++ + ++x + --x + x--;
+
+//         System.out.println("x = " + x + ", y = " + y);
+//     }
+// }
