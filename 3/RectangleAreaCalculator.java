5d1f296cafe98882497d1eab293bfc43cff952d2
NAVI-SL <navithanavindul@gmail.com>
2026-05-12 16:03:21 +0530

    Remove C1 class and its methods from RectangleAreaCalculator.java

--- 3/RectangleAreaCalculator.java
+++ 3/RectangleAreaCalculator.java
@@ -14,17 +14,3 @@ public class RectangleAreaCalculator {
         System.out.println("Area of the rectangle: " + rectangle1.calculateArea());
     }
 }
-public class C1 {
-    int n1,n2;
-    C1(){
-
-    }
-    int addNum(int n1,int n2){
-        return n1+n2;
-    }
-    public static void main(String[] args){
-        C1 c1 = new C1();
-        System.out.println("Sum is "+ c1.addNum(10,20));
-    }
-
-}
