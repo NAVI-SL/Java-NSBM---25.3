513cc8d8216f92c4fad7eb9d63d351b6c9a7df47
NAVI-SL <navithanavindul@gmail.com>
2026-05-27 10:50:05 +0530

    Add blockTraffic method to Firewall class for traffic management

--- Lec/INHERITANCE in Java/Take Home Activity 06/Q2/Firewall.java
+++ Lec/INHERITANCE in Java/Take Home Activity 06/Q2/Firewall.java
@@ -1,6 +1,9 @@
 package Q2;
 public class Firewall extends NetworkDevice{
     String firewallName;
-    
+
+    public void blockTraffic(String source) {
+        System.out.println(firewallName + "");
+    }
     
 }
