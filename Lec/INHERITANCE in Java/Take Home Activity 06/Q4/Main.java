c10a51c877c57b6d6b2ba95e475b2b02fc6a9a76
NAVI-SL <navithanavindul@gmail.com>
2026-06-03 13:22:07 +0530

    Enhance Main class to demonstrate AutonomousVehicle functionality with move, showBattery, chargeBattery, and enableAutopilot methods

--- Lec/INHERITANCE in Java/Take Home Activity 06/Q4/Main.java
+++ Lec/INHERITANCE in Java/Take Home Activity 06/Q4/Main.java
@@ -6,5 +6,9 @@ public class Main {
         a1.make = "Tesla";
         a1.speed = 120;
         a1.batteryLevel = 45;
+        a1.move();
+        a1.showBattery();
+        a1.chargeBattery();
+        a1.enableAutopilot();
     }
 }
