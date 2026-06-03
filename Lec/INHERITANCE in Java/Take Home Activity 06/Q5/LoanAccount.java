d1862cb8f6339690f0c791f62e2c79eaf54233e1
NAVI-SL <navithanavindul@gmail.com>
2026-06-03 13:38:35 +0530

    Implement LoanAccount class and enhance SavingsAccount with inheritance and interest application method

--- Lec/INHERITANCE in Java/Take Home Activity 06/Q5/LoanAccount.java
+++ Lec/INHERITANCE in Java/Take Home Activity 06/Q5/LoanAccount.java
@@ -1,5 +1,7 @@
 package Q5;
 
 public class LoanAccount {
+    double loanAmount;
+
     
 }
--- Lec/INHERITANCE in Java/Take Home Activity 06/Q5/SavingsAccount.java
+++ Lec/INHERITANCE in Java/Take Home Activity 06/Q5/SavingsAccount.java
@@ -1,5 +1,10 @@
 package Q5;
 
-public class SavingsAccount {
-    
+public class SavingsAccount extends Account {
+    double interestRate;
+
+    public void applyInterest() {
+        balance += (balance * interestRate / 100);
+        System.out.println("Interest applied. New balance: " + balance);
+    }
 }
