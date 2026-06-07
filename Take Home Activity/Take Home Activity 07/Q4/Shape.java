fb7513535042618c2091e0b61ed533d1d050555a
Navitha Navindul <navithanavindul@gmail.com>
2026-06-07 20:26:35 +0530

    Add draw and area methods to Shape class; create Circle class

--- Take Home Activity/Take Home Activity 07/Q4/Shape.java
+++ Take Home Activity/Take Home Activity 07/Q4/Shape.java
@@ -1,5 +1,11 @@
 package Q4;
 
 public class Shape {
-    
+    void draw() {
+        System.out.println("Drawing a generic shape");
+    }
+
+    void area() {
+        System.out.println("Area: 0.0");
+    }
 }
