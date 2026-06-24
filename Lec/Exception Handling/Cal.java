dada776e6ab4b0300649908ec6f8ded27f2ab092
NAVI-SL <navithanavindul@gmail.com>
2026-06-24 09:41:14 +0530

    Fix division by zero issue in Cal.java by adding input validation

--- Lec/Exception Handling/Cal.java
+++ Lec/Exception Handling/Cal.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class Cal {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your 1st Number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter your 2nd Number: ");
+        int num2 = sc.nextInt();
+
+        int ans = (num1/num2);
+        System.out.print("Answer is: " + ans);
+        
+        
+    }
 }
