92aabf2c77b4a67cab8f22be30517cdabb0ed15f
Navitha Navindul <navithanavindul@gmail.com>
2026-07-13 21:35:52 +0530

    Remove Lab 8 Java files for Q1, Q2, Q3, Q4, and Q5, including classes and main methods

--- Lab/8/Q2/Main.java
+++
@@ -1,9 +0,0 @@
-package Q2;
-
-public class Main {
-    public static void main(String[] args) {
-        Vehicle v1 = new Car(101, "Toyota", 120.5, 4);
-
-        v1.calculateSpeed();
-    }
-}
--- Lab/8/Q4/Main.java
+++
@@ -1,9 +0,0 @@
-package Q4;
-
-public class Main {
-    public static void main(String[] args) {
-        Manager m1 = new Manager(101, "Navitha", 150000, "IT");
-
-        m1.displayInfo();
-    }
-}
