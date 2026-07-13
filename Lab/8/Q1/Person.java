c96ab3dc73c71c471e95cd83151d4c35b958cbb2
Navitha Navindul <navithanavindul@gmail.com>
2026-07-13 21:55:24 +0530

    Fix Person class constructor and getter method; add Student class extending Person

---
+++ Lab/8/Q1/Person.java
@@ -0,0 +1,42 @@
+public class Person {
+	private int personID;
+	private String name;
+	private int age;
+	
+	public Person(int personID, Strinf name, int age) {
+		this.personID = personID;
+		this.name = name;
+		this.age = age;
+	}
+	
+	//Getter
+	public int getPersonID() {
+		return PersonID;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	//Setter
+	public void setPersonID(int personID) {
+		this.personID = personID;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+	
+	
+}
